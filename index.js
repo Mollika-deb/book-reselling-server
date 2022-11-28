7d3f477ceec97936a8daeaf28dce8ffda46b7d7f
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-28 21:35:50 +0600

    updated

--- index.js
+++ index.js
@@ -119,6 +119,14 @@ async function run() {
             res.send(users);
         });
 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email}
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'});
+        });
+       
+
         app.put('/users/admin/:id', verifyJWT, async(req, res) =>{
             const decodedEmail = req.decoded.email;
             const query = {email: decodedEmail};
