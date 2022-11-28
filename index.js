e31b370de2b009f17528ae955923eb352d42c5e5
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-28 16:15:29 +0600

    user get

--- index.js
+++ index.js
@@ -77,6 +77,26 @@ async function run() {
         });
         
 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+
+        app.put('/users/admin/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set: {
+                    role:'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+        
+
 
 
 
