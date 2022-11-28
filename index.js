b2bad3b65a25a2342e802731d8e26843162e346b
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-11-28 17:16:21 +0600

    token

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 //middleware
 app.use(cors());
